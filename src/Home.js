0dad85f080e699784448d00157cb7f96e2f43dd2
MMALARVIZHY <202100634@sigc.edu>
2024-03-11 05:56:10 -0700

    hh

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://thumbs.dreamstime.com/z/multi-ethnic-group-people-blog-concept-41494570.jpg?ct=jpeg" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://blog-backend-kohl.vercel.app"
